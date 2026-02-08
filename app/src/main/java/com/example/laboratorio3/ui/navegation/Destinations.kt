885c9b1298103d11e32619fc2762a82253380aaf
Geoordonez <geovanni.ordonez@galileo.edu>
2026-02-08 01:07:40 -0600

    creacion de Destinations.kt

---
+++ app/src/main/java/com/example/laboratorio3/ui/navegation/Destinations.kt
@@ -0,0 +1,9 @@
+package com.example.laboratorio3.ui.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object HomeDestination // Ruta para la pantalla principal [cite: 99]
+
+@Serializable
+data object HighlightsDestination // Nueva ruta para favoritos
